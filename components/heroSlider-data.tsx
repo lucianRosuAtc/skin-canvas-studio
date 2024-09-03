0a676e68e6b13873091e5f818c54e6d8fd811675
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-09-03 18:31:57 +0100

    Update HeroSlider images and descriptions

--- components/heroSlider-data.tsx
+++ components/heroSlider-data.tsx
@@ -1,32 +1,28 @@
 export const images = [
     {
-        url: 'https://i.ibb.co/qCkd9jS/img1.jpg',
-        name: 'Switzerland',
+        // url: '/img/asian/asian arm tattoo.jpg',
+        url: '/img/asian/asianArmTatoo.jpg',
+        name: 'Asian',
         description: 'Welcome to SkinCanvas Studio - Where Art Meets Skin. Transform Your Body into a Living Masterpiece with Our Award-Winning Artists. Precision, Passion, and Creativity in Every Stroke.',
     },
     {
-        url: 'https://i.ibb.co/jrRb11q/img2.jpg',
-        name: 'Finland',
-        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id optio qui nulla vero! Voluptatem a obcaecati ducimus veritatis sunt fugiat!',
+        url: '/img/bigCat/tiger-tattoo-realism.jpg',
+        name: 'Realism Big Cat',
+        description: 'Big Cat realism tattoo. Our artist are professional and have a lot of experience in this style. We are the best in the city.',
     },
     {
-        url: 'https://i.ibb.co/NSwVv8D/img3.jpg',
-        name: 'Iceland',
-        description: 'Delectus ut quam neque labore officia. Minus iste explicabo veniam architecto laborum, dolorem dolor excepturi!',
+        url: '/img/realism/realismMecanic.webp',
+        name: 'Realism Mecanism',
+        description: 'Realism Mecanical tattoo. Intricate and detailed work, complex and beautiful shading. Mecanical dissign are intertuailing with the skin.',
     },
     {
-        url: 'https://i.ibb.co/Bq4Q0M8/img4.jpg',
-        name: 'Australia',
-        description: 'Mollitia dolore amet ab. Atque eum dolorem eligendi! Facere quibusdam autem consectetur ab labore. Labore perferendis corrupti expedita quos, accusantium in laudantium error itaque.',
+        url: '/img/werewolf/werewolf-scary.jpg',
+        name: 'Werewolf',
+        description: 'WereWolf realism tattoo. This futuristic style is a mix of realism and fantasy. Is perfect for those who want to show their wild side.',
     },
     {
-        url: 'https://i.ibb.co/jTQfmTq/img5.jpg',
-        name: 'Netherland',
-        description: 'Qui esse expedita praesentium repellat quis fugiat accusamus aspernatur consequuntur nihil quae dicta quod aliquam recusandae placeat vitae.',
+        url: '/img/moon/blood-moon-and-stars.jpg',
+        name: 'Blood Moon realism tattoo',
+        description: 'Blood Moon realism tattoo. Another futuristic style that is a mix of realism and fantasy. A original aproach to the moon for those atracted by the mistery of the night.',
     },
-    // {
-    //     url: 'https://i.ibb.co/RNkk6L0/img6.jpg',
-    //     name: 'Ireland',
-    //     description: 'Reprehenderit cumque deserunt? Asperiores nostrum mollitia modi ipsam nam, magni assumenda nobis blanditiis.reprehenderit cumque deserunt? Asperiores nostrum mollitia modi ipsam nam, magni assumenda.',
-    // },
 ];
