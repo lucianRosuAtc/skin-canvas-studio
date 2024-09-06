ea75323654464b5b3c61092ddb2659da7b5f7455
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-09-06 17:00:08 +0100

    Refactor Hero component: Comment out unused code

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,7 @@ export default function Hero() {
         </div>
       ))}
 
-      <div className="max-w-screen-2xl mx-auto border border-gray-200 rounded-lg shadow-lg my-20">
+      {/* <div className="max-w-screen-2xl mx-auto border border-gray-200 rounded-lg shadow-lg my-20">
         <h2 className="pl-10 pt-10 text-bold text-5xl">Featured Artists</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-10 gap-3">
           <Image src="/img/eyes/greeneyes2.jpg" alt="tattoo" width={160} height={120} className="h-40 w-52" />
@@ -52,7 +52,7 @@ export default function Hero() {
             Hours: Monday - Saturday, 10:00 AM - 8:00 PM
           </p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
