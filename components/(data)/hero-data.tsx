1ac1d0c5bb8edc07e9b432576824d0c9807ea066
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-09-06 18:27:51 +0100

    changing the v0 to a dinamically way to load data

--- components/(data)/hero-data.tsx
+++ components/(data)/hero-data.tsx
@@ -1,18 +1,38 @@
 export const heroData = [
     {
-        title:'Discover Talent',
-        desc:' Browse through our curated selection of top tattoo artists from around the world.',
+        title: 'Discover Talent',
+        desc: ' Browse through our curated selection of top tattoo artists from around the world.',
     },
     {
-        title:'Connect Directly',
-        desc:'Message artists, discuss your ideas, and book appointments all in one place.',
+        title: 'Connect Directly',
+        desc: 'Message artists, discuss your ideas, and book appointments all in one place.',
     },
     {
-        title:'Virtual Tattoo Try-On',
-        desc:'Use our AR technology to see how a tattoo might look on your body before committing.',
+        title: 'Virtual Tattoo Try-On',
+        desc: 'Use our AR technology to see how a tattoo might look on your body before committing.',
     },
     {
-        title:'Community Showcase',
-        desc:'Share your ink and get inspired by other tattoo enthusiasts in our vibrant community.',
+        title: 'Community Showcase',
+        desc: 'Share your ink and get inspired by other tattoo enthusiasts in our vibrant community.',
+    }
+]
+
+
+export const artistsData = [
+    {
+        image: '/img/eyes/greeneyes2.jpg',
+        desc: 'Our Story InkFlow was born out of a passion for tattoo artistry and a desire to create a platform that celebrates the diverse world of body art. Founded in 2018 by a group of tattoo enthusiasts and artists, our mission is to bridge the gap between talented tattoo artists and those seeking unique, personalized body art. What started as a small online gallery has grown into a thriving community, connecting artists and clients from around the globe. We believe that every tattoo tells a story, and we&apos;re here to help those stories come to life.',
+    },
+    {
+        image: '/img/eyes/greeneyes2.jpg',
+        desc: '2Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam inventore aut ducimus nostrum, culpa aperiam mollitia harum enim delectus facere.',
+    },
+    {
+        image: '/img/eyes/greeneyes2.jpg',
+        desc: '3Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam inventore aut ducimus nostrum, culpa aperiam mollitia harum enim delectus facere.',
+    },
+    {
+        image: '/img/eyes/greeneyes2.jpg',
+        desc: "Get in Touch Have a question or ready to schedule your next tattoo? We'd love to hear from you! Address: 123 Ink Street, Tattoo City, TC 12345 Phone: (555) 123-4567 Email: info@inkflow.io Hours: Monday - Saturday, 10:00 AM - 8:00 PM",
     }
 ]
