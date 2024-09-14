23e3b210c9b0f7af42870aeec2c6769892bc53ae
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-09-14 18:48:32 +0100

    Enhance CTA component with new design and link, update GalleryTabs layout for improved responsiveness

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 export default function CTA() {
   return (
-    <div>
-      Nu fi bulangiu, Tatueaza-te!
+    <div className="bg-gray-800 text-white py-12 px-6 text-center rounded-lg shadow-lg">
+      <h2 className="text-3xl font-bold mb-4">Convert your idea into art</h2>
+      <p className="text-lg mb-6">Transform your body into a canvas of art. Book your appointment today and get inked by the best artists in town!</p>
+      <Link href="/contact" className="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-full transition duration-300">
+          Book an Appointment
+        
+      </Link>
     </div>
-  )
+  );
 }
--- components/GalleryTabs.tsx
+++ components/GalleryTabs.tsx
@@ -21,15 +21,13 @@ export default function Gallery() {
 
   return (
     <section className="min-h-screen pt-12">
-      <div className="px-8 mx-auto">
+      <div className="px-4 mx-auto">
         <h2 className=" text-center mx-auto">Gallery</h2>
         <div className="text-center max-w-lg mx-auto py-10">
           <h4 className="text-xl mb-2">Welcome to Skin Canvas Studio</h4>
           <p className="text-base">Our gallery features a collection of tattoo designs from our talented artists. Browse through our gallery to find inspiration for your next tattoo.</p>
         </div>
 
-
-        
         <Tabs defaultValue={category} className="mb-12 xl:mb-24">
           <TabsList className="grid md:grid-cols-5 w-full max-w-[900px] h-full mb-12 mx-auto md:border dark:border-none ">
             {categories.map((category, index) => {
@@ -48,14 +46,11 @@ export default function Gallery() {
           <div className="grid grid-cols-1 w-full mx-auto md:gap-5">
             {filteredProjects.map((project, index) => (
               <div key={index}>
-
                 <div className="mx-auto w-full shadow-xl dark:shadow-secondary dark:shadow-lg border max-w-[1520px] mb-16 rounded-xl">
-                  
                   <h2 className="px-4 pt-10 text-center">{project.name}</h2>
                   <p className="text-center text-muted-foreground text-lg font-light p-5">
                     {project.description}
                   </p>
-
                   <div className="mx-auto pb-10">
                     {project.gridLayout}
                   </div>
