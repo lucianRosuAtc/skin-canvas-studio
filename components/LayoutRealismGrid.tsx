ea0cc3f7c182c1d4c2cb49b8618613263c6546f5
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-09-17 12:33:18 +0100

    Add SkeletonFive and SkeletonSix components; update cards data with realism-themed thumbnails and adjust layout classes

--- components/LayoutRealismGrid.tsx
+++ components/LayoutRealismGrid.tsx
@@ -69,6 +69,35 @@ const SkeletonFour = () => {
   );
 };
 
+const SkeletonFive = () => {
+  return (
+    <div>
+      <p className="font-bold md:text-4xl text-xl text-white">
+        Full Back and Neck Geometric Tattoo
+      </p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A full back and neck tattoo with geometric patterns that are perfect for
+        those who love clean lines and symmetry
+      </p>
+    </div>
+  );
+};
+const SkeletonSix = () => {
+  return (
+    <div>
+      <p className="font-bold md:text-4xl text-xl text-white">
+        Full Back and Neck Geometric Tattoo
+      </p>
+      <p className="font-normal text-base text-white"></p>
+      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+        A full back and neck tattoo with geometric patterns that are perfect for
+        those who love clean lines and symmetry
+      </p>
+    </div>
+  );
+};
+
 
 // MARK: Cards data
 
@@ -76,30 +105,45 @@ const cards = [
   {
     id: 1,
     content: <SkeletonOne />,
-    className: "md:col-span-2",
+    className: "md:col-span-1",
     thumbnail:
-      "/img/geometric/geometric tatoo.webp",
+      "/img/realism/realism-arm-flowers.jpg",
   },
   {
     id: 2,
     content: <SkeletonTwo />,
     className: "col-span-1",
     thumbnail:
-      "/img/geometric/geometric back.jpg",
+      "/img/realism/realism-skul.jpg",
   },
   {
     id: 3,
     content: <SkeletonThree />,
     className: "col-span-1",
     thumbnail:
-      "/img/geometric/geometric-IV.jpg",
+      "/img/realism/realismFlower.jpg",
   },
   {
     id: 4,
     content: <SkeletonFour />,
-    className: "md:col-span-2",
+    className: "md:col-span-1",
     thumbnail:
-      "/img/geometric/geometric1.webp",
+    "/img/realism/realism-snake.jpg",
+  },
+  {
+    id: 5,
+    content: <SkeletonFive />,
+    className: "md:col-span-1",
+    thumbnail:
+    
+    "/img/realism/realismFlowersAndMoon.webp",
+  },
+  {
+    id: 6,
+    content: <SkeletonSix />,
+    className: "md:col-span-1",
+    thumbnail:
+    "/img/realism/realism.jpg",
   },
 
 ];
