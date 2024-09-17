b393f08c3849203f6a96d265c378b1f9202056e3
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-09-17 14:29:14 +0100

    Update about-data descriptions and enhance TimelineComp layout for better readability

--- components/(data)/about-data.tsx
+++ components/(data)/about-data.tsx
@@ -30,39 +30,39 @@ export const ourTeam = [
 export const ourJourney2024 = [
     {
         image: "/img/ourJourney/Lion1.webp",
-        desc: "Lorem ipsum dolor sit.",
+        desc: "The year 2024 marks the beginning of a new era for Skin Canvas Studio. This is the tattoo of the year.",
     },
     {
-        image: "/img/bigCat/Lion1.webp",
-        desc: "Lorem ipsum dolor sit.",
+        image: "/img/ourJourney/asian-VI.webp",
+        desc: "Full back tatoo of asian deity with a high level of detail and realism.",
     },
     {
-        image: "/img/bigCat/Lion1.webp",
-        desc: "Lorem ipsum dolor sit.",
+        image: "/img/ourJourney/asian arm tatoo.webp",
+        desc: "Asian arm tatoo with a high level of detail and showing the artist's skill in the traditional style.",
     },
     {
-        image: "/img/bigCat/Lion1.webp",
-        desc: "Lorem ipsum dolor sit.",
+        image: "/img/ourJourney/geometric tatoo.webp",
+        desc: "Geometric tatoo with a floral pattern falloing the sacred geometry principles.",
     },
 ]
 
 // MARK: Our Journey 2023 
 export const ourJourney2023 = [
     {
-        image: "/img/bigCat/tigerRealism.jpg",
-        desc: "Lorem ipsum dolor sit.",
+        image: "/img/ourJourney/tiger-tattoo-realism.jpg",
+        desc: "The tattoo of the year 2023 voted for more than 1000 clients.",
     },
     {
-        image: "/img/bigCat/tigerRealism.jpg",
-        desc: "Lorem ipsum dolor sit.",
+        image: "/img/ourJourney/realism-skul.jpg",
+        desc: "Realism skull tatoo with a high level of detail and Maya culrural references.",
     },
     {
-        image: "/img/bigCat/tigerRealism.jpg",
-        desc: "Lorem ipsum dolor sit.",
+        image: "/img/ourJourney/werewolf scary.webp",
+        desc: "Werewolf tatoo with a high level of detail and a scary look.",
     },
     {
-        image: "/img/bigCat/tigerRealism.jpg",
-        desc: "Lorem ipsum dolor sit.",
+        image: "/img/ourJourney/geometric1.webp",
+        desc: "Geometric tatoo fallowing the sacred geometry principles.",
     },
 ]
 
