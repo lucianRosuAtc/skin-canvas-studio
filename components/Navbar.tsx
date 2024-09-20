f93f51bb0a03a629bbdc52fd517f9913687a49af
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-09-20 16:55:39 +0100

    Refactor Navbar component; implement mobile menu functionality and improve layout for better responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,36 +1,93 @@
 'use client';
 import { usePathname } from "next/navigation";
-import React from 'react'
-import ThemeToggler from './ThemeToggler'
-import Link from 'next/link'
+import React, { useState } from 'react';
+import ThemeToggler from './ThemeToggler';
+import Link from 'next/link';
 import { navigationlinks } from "./(data)/navigationlinks-data";
 import Image from "next/image";
+import { Menu, X } from 'lucide-react';
+import { lora } from "./fonts";
+import { FloatingDockDemo } from "./FloatingDock";
 
 export default function Navbar() {
   const pathname = usePathname();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <div className="hidden md:flex justify-between light:bg-gray-200 h-24 border-b-2 border-b-primary ">
+    <>
+      <div className="hidden md:flex justify-between light:bg-gray-200 h-24 border-b-2 border-b-primary">
         <Link href="/" className="flex ml-16 items-center justify-center">
-          <Image src="/img/bigCats/Lion1.webp"
+          <Image
+            src="/img/bigCats/Lion1.webp"
             width={70}
             height={70}
             alt="Skin Canvas Studio Logo"
-            className="flex mx-auto rounded-full border-2 border-primary shadow-xl" />
+            className="flex mx-auto rounded-full border-2 border-primary shadow-xl"
+          />
         </Link>
-      <div className="flex my-auto ml-20 space-x-6">
-        {navigationlinks.map((navlink) => (
-          <Link className={`link hover:text-primary pb-1 ${pathname === navlink.url ? 'border-b-2 border-b-primary transition-all' : ''}`}
-            key={navlink.url}
-            href={navlink.url}
-          >
-            {navlink.name}
-          </Link>
-        ))}
+        <div className="flex my-auto ml-20 space-x-6">
+          {navigationlinks.map((navlink) => (
+            <Link
+              className={`link hover:text-primary pb-1 ${pathname === navlink.url ? 'border-b-2 border-b-primary transition-all' : ''}`}
+              key={navlink.url}
+              href={navlink.url}
+            >
+              {navlink.name}
+            </Link>
+          ))}
+        </div>
+        <div className="pr-20 my-auto">
+          <ThemeToggler />
+        </div>
       </div>
-      <div className="pr-20 my-auto">
-        <ThemeToggler />
-      </div>
-    </div>
-  )
-}
 
+      {/* Mobile Navbar */}
+      <div className="flex md:hidden justify-between items-center px-4 py-1 border-b-2 border-b-primary">
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/img/bigCats/Lion1.webp"
+            width={50}
+            height={50}
+            alt="Skin Canvas Studio Logo"
+            className="rounded-full border-2 border-primary shadow-xl"
+          />
+        </Link>
+        <div className="flex space-x-5">
+          <div className="py-4">
+            <ThemeToggler />
+          </div>
+          <button onClick={toggleMobileMenu} className="text-primary focus:outline-none">
+            {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
+          </button>
+        </div>
+      </div>
+
+      <div
+        className={`fixed top-21 left-0 w-full h-full bg-inherit z-50 transform transition-transform  ease-in-out ${
+          isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
+      >
+        <div className="flex flex-col items-start m-8">
+          {navigationlinks.map((navlink) => (
+            <Link
+              className={`text-2xl font-black ${lora.className} hover:text-primary py-2 ${pathname === navlink.url ? 'border-b-2 border-b-primary transition-all' : ''}`}
+              key={navlink.url}
+              href={navlink.url}
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              {navlink.name}
+            </Link>
+          ))}
+        </div>
+        <div className="mt-16">
+
+        <FloatingDockDemo />
+        </div>
+      </div>
+    </>
+  );
+}
