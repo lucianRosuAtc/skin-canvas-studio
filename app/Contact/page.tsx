2dcf259ff9a338f62d54f7bd2958e35ca4fbbff7
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-09-20 11:53:21 +0100

    Update Contact page layout and content; enhance accessibility and readability; improve text alignment and styling

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -7,24 +7,30 @@ export default function Contact() {
     <div className="max-w-7xl mx-auto px-8">
       <div className="grid pt-12 mb-6 xl:mb-16">
         <div className="flex flex-col justify-center">
-          <div className="flex items-center mx-auto gap-x-4 text-primary text-lg lg:text-2xl mb-4">
-            Get in Touch with Me
+          <div className="flex items-center justify-center text-primary text-lg lg:text-2xl mb-4">
+            Get in Touch with Skin Canvas Studio
           </div>
 
           <div className="text-center">
-            <h1 className="my-4 mx-auto mb-20">Hire Me</h1>
+            <h1 className="my-4 mx-auto mb-10 md:mb-20">Contact Us</h1>
           </div>
 
           <div className="flex flex-col md:flex-row mb-8">
             <div className="flex flex-col">
-              <p className="max-w-[600px] md:mr-6 mb-4 md:mb-10">
-              Get in Touch Have a question or ready to schedule your next tattoo? We&apos;d love to hear from you!  Hours: Monday - Saturday, 10:00 AM - 8:00 PM
+              <p className="max-w-[600px] md:mr-6 mb-2 text-sm md:text-base">
+                We&apos;d love to hear from you!
               </p>
-            
+              <p className="max-w-[600px] md:mr-6 mb-2 text-sm md:text-base">
+                Whether you have a question about our services, pricing, or anything else, our team is ready to answer all your questions.
+              </p>
+              <p className="max-w-[600px] md:mr-6 mb-3 md:mb-16 text-sm md:text-base">
+                Fill out the form and we&apos;ll get back to you as soon as possible.
+              </p>
+
 
               <div className="flex flex-col my-8 md:my-0 gap-y-6 xl:gap-y-8 xl:text-lg">
 
-                <div className="flex items-center gap-x-8 ">
+              <div className="flex items-center gap-x-3 md:gap-x-6 text-sm md:text-base">
                   <MailIcon size={18} className="text-primary" />
                   <a
                     href="mailto:vigilent.developer@gmail.com"
@@ -34,17 +40,17 @@ export default function Contact() {
                   </a>
                 </div>
 
-                <div className="flex items-center gap-x-8">
+                <div className="flex items-center gap-x-3 md:gap-x-6 text-sm md:text-base">
                   <HomeIcon size={18} className="text-primary" />
-                  <div className="">Address: 190 High St, Exeter EX4 3DU </div>
+                  <p>190 High St, Exeter EX4 3DU </p>
                 </div>
 
-                <div className="flex items-center gap-x-8">
+                <div className="flex items-center gap-x-3 md:gap-x-6 text-sm md:text-base">
                   <Clock size={18} className="text-primary" />
-                  <div className=""> Hours: Monday - Saturday, 10:00 AM - 8:00 PM </div>
+                  <p> Mon - Sat, 10:00 AM - 8:00 PM </p>
                 </div>
 
-                <div className="flex items-center gap-x-8">
+                <div className="flex items-center gap-x-3 md:gap-x-6 text-sm md:text-base">
                   <PhoneCall size={18} className="text-primary" />
                   <a href="tel:07505xxxxxx" className="hover:text-primary">
                     07505700000
